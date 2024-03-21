9b93953b76aed44b3895fd46e829e25f20c2ac47
Tatiana Cuadros <tatianacuadros31@gmail.com>
2024-03-21 18:03:57 -0500

    Create restrictions and improve the code.

--- encryption-decryption.js
+++ encryption-decryption.js
@@ -1,57 +1,41 @@
-/* La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" */
-
+let regEx = /^[a-z\s]*$/;
 
 function encryption() {
     let text = document.getElementById("textToEncrypt").value;
     let resultEncrypt = " ";
 
-    for (let character of text) {
-        if (character === "a") {
-            resultEncrypt += character.replaceAll("a", "ai");
-        } else if (character === "e") {
-            resultEncrypt += character.replaceAll("e", "enter")
-        } else if (character === "i") {
-            resultEncrypt += character.replaceAll("i", "imes")
-        } else if (character === "o") {
-            resultEncrypt += character.replaceAll("o", "ober")
-        } else if (character === "u") {
-            resultEncrypt += character.replaceAll("u", "ufat")
+    if (!regEx.test(text) || text.length <= 0) {
+        alert("Error")
+    } else {
+        for (let character of text) {
+            if (character === "a") {
+                resultEncrypt += character.replaceAll("a", "ai");
+            } else if (character === "e") {
+                resultEncrypt += character.replaceAll("e", "enter")
+            } else if (character === "i") {
+                resultEncrypt += character.replaceAll("i", "imes")
+            } else if (character === "o") {
+                resultEncrypt += character.replaceAll("o", "ober")
+            } else if (character === "u") {
+                resultEncrypt += character.replaceAll("u", "ufat")
+            } else {
+                resultEncrypt += character;
+            }
         }
-        else {
-            resultEncrypt += character;
-        }
+        document.getElementById("message").textContent = "Encrypted Text!🎉​";
+        document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${resultEncrypt}</h3>`;
     }
-
-    document.getElementById("message").textContent = "Encrypted Text!🎉​";
-    document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${resultEncrypt}</h3>`;
 }
 
 function decipher() {
     let text = document.getElementById("textToEncrypt").value;
-    
-    if(text.includes("ai")){
-        text = text.replaceAll("ai", "a");
-    }
-    if(text.includes("enter")){
-        text = text.replaceAll("enter", "e");
-    }
-    if(text.includes("imes")){
-        text = text.replaceAll("imes", "i");
-    }
-    if(text.includes("ober")){
-        text = text.replaceAll("ober", "o");
-    }
-    if (text.includes("ufat")) {
-        text = text.replaceAll("ufat", "u")
-    } else {
-        text = text;
-    }
+
+    text = text.replaceAll("ai", "a");
+    text = text.replaceAll("enter", "e");
+    text = text.replaceAll("imes", "i");
+    text = text.replaceAll("ober", "o");
+    text = text.replaceAll("ufat", "u");
 
     document.getElementById("message").textContent = "Decrypted Text!🎉​";
     document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${text}</h3>`;
 }
-
