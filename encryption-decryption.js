d4bccadac66d3baebb23fd83ef2ee24ecf5b3b07
Tatiana Cuadros <tatianacuadros31@gmail.com>
2024-03-20 21:49:01 -0500

    initiated with text decipher logic

--- encryption-decryption.js
+++ encryption-decryption.js
@@ -4,6 +4,7 @@ La letra "a" es convertida para "ai"
 La letra "o" es convertida para "ober"
 La letra "u" es convertida para "ufat" */
 
+
 function encryption() {
     let text = document.getElementById("textToEncrypt").value;
     let resultEncrypt = " ";
@@ -27,3 +28,26 @@ function encryption() {
     console.log(resultEncrypt);
 }
 
+function decipher() {
+    let text = document.getElementById("textToEncrypt").value;
+    
+    if(text.includes("ai")){
+        text = text.replaceAll("ai", "a");
+    }
+    if(text.includes("enter")){
+        text = text.replaceAll("enter", "e");
+    }
+    if(text.includes("imes")){
+        text = text.replaceAll("imes", "i");
+    }
+    if(text.includes("ober")){
+        text = text.replaceAll("ober", "o");
+    }
+    if (text.includes("ufat")) {
+        text = text.replaceAll("ufat", "u")
+    } else {
+        text = text;
+    }
+    console.log(text);
+}
+decipher()
