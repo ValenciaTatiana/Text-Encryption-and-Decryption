4562f4987775f2b62124a261ff5d8b697732e9be
Tatiana Cuadros <tatianacuadros31@gmail.com>
2024-03-22 15:25:47 -0500

    Logic of the copy text button

--- encryption-decryption.js
+++ encryption-decryption.js
@@ -3,13 +3,14 @@ let regEx = /^[a-z\s]*$/;
 function encryption() {
     let text = document.getElementById("textToEncrypt").value;
     let resultEncrypt = " ";
-    let alert = " ";
+    let alert = document.querySelector(".alert");
 
     if (!regEx.test(text) || text.length <= 0) {
-        alert = document.querySelector(".alert")
         alert.textContent = !regEx.test(text) ? "Uppercase letters and special characters are not allowed in this field." : "The field cannot be empty."
 
-        setTimeout(function() {
+        alert.style.display = "block";
+
+        setTimeout(function () {
             alert.style.display = "none";
         }, 2000)
     } else {
@@ -32,25 +33,43 @@ function encryption() {
         document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${resultEncrypt}</h3>`;
     }
 }
-
 function decipher() {
     let text = document.getElementById("textToEncrypt").value;
 
     if (!regEx.test(text) || text.length <= 0) {
         alert = document.querySelector(".alert")
         alert.textContent = !regEx.test(text) ? "Uppercase letters and special characters are not allowed in this field." : "The field cannot be empty."
-        
-        setTimeout(function() {
+
+        alert.style.display = "block";
+
+        setTimeout(function () {
             alert.style.display = "none";
         }, 2000)
     } else {
-    text = text.replaceAll("ai", "a");
-    text = text.replaceAll("enter", "e");
-    text = text.replaceAll("imes", "i");
-    text = text.replaceAll("ober", "o");
-    text = text.replaceAll("ufat", "u");
+        text = text.replaceAll("ai", "a");
+        text = text.replaceAll("enter", "e");
+        text = text.replaceAll("imes", "i");
+        text = text.replaceAll("ober", "o");
+        text = text.replaceAll("ufat", "u");
 
-    document.getElementById("message").textContent = "Decrypted Text!🎉​";
-    document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${text}</h3>`;
+        document.getElementById("message").textContent = "Decrypted Text!🎉​";
+        document.getElementById("displayEncrypted-menssage").innerHTML = `<h3 class="style-message">${text}</h3>`;
     }
+    
 }
+function copy() {
+    let textToCopy = document.getElementById("displayEncrypted-menssage");
+    
+    let tempTextArea = document.createElement("textarea");
+    tempTextArea.value = textToCopy.textContent;
+    
+    document.body.appendChild(tempTextArea);
+    
+    tempTextArea.select();
+    document.execCommand("copy"); 
+    
+    document.body.removeChild(tempTextArea);
+
+    console.log("Text copied to clipboard!", "success");
+}
+
